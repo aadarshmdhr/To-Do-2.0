166f2930fc26f499eef3920ad349d78e3fc522b2
Aadarsh Manandhar <aadarshmanandhar2@gmail.com>
2026-05-02 22:03:35 +0545

    Created update in todo list in views.py

--- TODO/urls.py
+++ TODO/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path("", views.todo_list),
     path("delete/<int:pk>/", views.todo_delete),
     path("create/", views.todo_create),
+    path("update/<int:pk>", views.todo_update),
 ]
